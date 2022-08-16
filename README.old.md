4550d7e7cb6adf98c6815e6c3d4073e516dfbd98
Zach Trani <zachtrani@Ariannas-MacBook-Pro.local>
2022-08-16 18:20:58 -0400

    React template

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# React-To-Do-List
+# react-to-do-list
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='header'>To Do List</h1>
+    <div className='input-container'>
+      <input className="user-input"></input>
+      <button className="button">Add</button>
+    </div>
+    </>
+  );
+}
+
+export default App;
