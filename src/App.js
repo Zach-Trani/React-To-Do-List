abe37e31ea0aca60af79b69a6ea75c99939b3f40
Zach Trani <zachtrani@Ariannas-MacBook-Pro.local>
2022-08-16 18:47:12 -0400

    logged user input on web console

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [input, setInput] = useState()
+
+  function handleClick() {
+    // This is js. Inspect page and see user input in terminal
+    console.log(input)
+  }
   return (
     <>
+    {/* This is JSX, see how comments are different */}
     <h1 className='header'>To Do List</h1>
     <div className='input-container'>
-      <input className="user-input"></input>
-      <button className="button">Add</button>
+      {/* e "event" sent to funtion setInput then stored in input variable */}
+      <input className="user-input" onChange={(e) => setInput(e)}></input>  
+      <button className="button" onClick={handleClick}>Add</button>
     </div>
     </>
   );
